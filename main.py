ab460eaff4d730cba68a3a70b0a57039fb8a0d7a
Jona Avalos <jonavalos17@gmail.com>
2024-10-11 10:34:40 -0600

    division clases, inicio mapa

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import math
-
 import pygame
 import random
+from Mapa import *
+# Dimensiones de las celdas y de la pantalla
+ANCHO_CELDA = 36
+ALTO_CELDA = 36
+MAPA_ANCHO = 1000
+MAPA_ALTO = 700
+
 
 # Initialize pygame
 pygame.init()
 
+
 # create screen
-screen = pygame.display.set_mode((1000, 750))
+screen = pygame.display.set_mode((MAPA_ANCHO, MAPA_ALTO))
 
 # background
 background = pygame.image.load('PNGs/background.png')
@@ -16,7 +23,6 @@ background = pygame.image.load('PNGs/background.png')
 pygame.display.set_caption("Pac-Man")
 icon = pygame.image.load('PNGs/game.png')
 pygame.display.set_icon(icon)
-
 
 
 # game loop
@@ -31,17 +37,17 @@ while running:  # agarra todos los eventos ingame uno por uno en el for y checke
             running = False
 
 
+    # Función para dibujar el mapa en pantalla
+    def dibujar_mapa(mapa):
+        for y, fila in enumerate(mapa):
+            for x, celda in enumerate(fila):
+                color = (0, 0, 255) if celda.valor == 'pared' else (255, 255, 255)  # Azul para pared, blanco para otros
+                pygame.draw.rect(screen, color, pygame.Rect(x * ANCHO_CELDA, y * ALTO_CELDA, ANCHO_CELDA, ALTO_CELDA))
 
 
 
-
-
-
-
-
-
-
-
-
+    # Llamar la función dentro de un bucle de Pygame
+    dibujar_mapa(mapa)
+    pygame.display.flip()  # Actualizar la pantalla
 
 print ("hello world!")
